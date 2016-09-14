71eb5a82d34acdeb88b4bb86c8641d27f5886e0c
Jonathan Oates <oates999@gmail.com>
2016-09-14 17:07:42 -0400

    finished tilemap for level 3

---
+++ FinalAlchemist/assets/data/tilemaps/Basic Tiles.tsx
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Basic Tiles" tilewidth="50" tileheight="50" tilecount="27" columns="0">
+ <tile id="0">
+  <image width="50" height="50" source="../../images/tile_set/Broken_Floor_Left.png"/>
+ </tile>
+ <tile id="1">
+  <image width="50" height="50" source="../../images/tile_set/Broken_Floor_Right.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="../../images/tile_set/cinderblocks_cornerleft.png"/>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="../../images/tile_set/cinderblocks_cornerright.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="../../images/tile_set/cinderblocks_mid.png"/>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="../../images/tile_set/Corner_Floor_Left.png"/>
+ </tile>
+ <tile id="6">
+  <image width="50" height="50" source="../../images/tile_set/Corner_Floor_Right.png"/>
+ </tile>
+ <tile id="7">
+  <image width="50" height="50" source="../../images/tile_set/cross_solo.png"/>
+ </tile>
+ <tile id="8">
+  <image width="50" height="50" source="../../images/tile_set/crosshatch_4x.png"/>
+ </tile>
+ <tile id="9">
+  <image width="50" height="50" source="../../images/tile_set/crosshatch_up2x.png"/>
+ </tile>
+ <tile id="10">
+  <image width="50" height="50" source="../../images/tile_set/floor_topleft.png"/>
+ </tile>
+ <tile id="11">
+  <image width="50" height="50" source="../../images/tile_set/floor_topright.png"/>
+ </tile>
+ <tile id="12">
+  <image width="50" height="50" source="../../images/tile_set/greypanel_mid.png"/>
+ </tile>
+ <tile id="13">
+  <image width="50" height="50" source="../../images/tile_set/greypanel_solo.png"/>
+ </tile>
+ <tile id="14">
+  <image width="50" height="50" source="../../images/tile_set/Inner_Floor.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="3" y="2" width="47" height="47"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <image width="50" height="50" source="../../images/tile_set/light_left.png"/>
+ </tile>
+ <tile id="16">
+  <image width="50" height="50" source="../../images/tile_set/light_mid.png"/>
+ </tile>
+ <tile id="17">
+  <image width="50" height="50" source="../../images/tile_set/light_right.png"/>
+ </tile>
+ <tile id="18">
+  <image width="50" height="50" source="../../images/tile_set/lighttile_solo.png"/>
+ </tile>
+ <tile id="19">
+  <image width="50" height="50" source="../../images/tile_set/Regular_Floor_1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="50" height="50" source="../../images/tile_set/rubbleblocks_mid.png"/>
+ </tile>
+ <tile id="21">
+  <image width="50" height="50" source="../../images/tile_set/smalltile_solo.png"/>
+ </tile>
+ <tile id="22">
+  <image width="50" height="50" source="../../images/tile_set/sturdypanel_solo.png"/>
+ </tile>
+ <tile id="23">
+  <image width="50" height="50" source="../../images/tile_set/sturdypanel_uplink.png"/>
+ </tile>
+ <tile id="24">
+  <image width="50" height="50" source="../../images/tile_set/elevatorwall_left.png"/>
+ </tile>
+ <tile id="25">
+  <image width="50" height="50" source="../../images/tile_set/elevatorwall_mid.png"/>
+ </tile>
+ <tile id="26">
+  <image width="50" height="50" source="../../images/tile_set/elevatorwall_right.png"/>
+ </tile>
+</tileset>
