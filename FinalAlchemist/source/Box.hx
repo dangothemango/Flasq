6a3b76f8d8d49b7688f1839b36ceacdea17d9465
Pasha/Paul Chorba <ninjadinosir@gmail.com>
2016-09-20 23:06:53 -0400

    oh gawd

--- FinalAlchemist/source/Box.hx
+++ FinalAlchemist/source/Box.hx
@@ -8,45 +8,33 @@ import flixel.tweens.FlxTween;
 
 class Box extends FlxSprite
 {
-
-    var oHeight:Float;
+	var player:Player;
 
     public function new(?X:Float=0, ?Y:Float=0, ?W:Int=10,?H:Int=10)
     {
-        super(X,Y,W,H);
-        range=150;
-        makeGraphic(W,H,0xFF4d4d4d);
-        oHeight=H;
-        
+        super(X,Y);
+		x=X;
+        y=Y;
+		loadBox();
+		setGraphicSize(W);
+		updateHitbox();
+		drag.x = 1000;
+		solid = true;
+		acceleration.y = 1;
+		immovable = false;
     }
 
-    private function tweenFunction(s:FlxSprite, v:Float) { 
-        s.setGraphicSize(Std.int(s.width),Std.int(v)); 
-        updateHitbox();
+	public function attachPlayer(p:Player){
+        player=p;
     }
-
-    function tweenDriver(s:Float,e:Float){
-        FlxTween.num(s, e, .25, {}, tweenFunction.bind(this));
-    }
-
-    override function inRange(elapsed:Float){
-        if (withinRange){
-            return;
-        }
-        withinRange=true;
-        tweenDriver(this.height,1);
-    }
-
-    override function outOfRange(){
-        if (!withinRange){
-            return;
-        }
-        withinRange=false;
-        tweenDriver(this.height,oHeight);
-    }
-
+	
+	function loadBox(){
+		loadGraphic(AssetPaths.crate__png, false, 245, 245);
+	}
+    
     override public function update(elapsed:Float):Void
     {
+		trace(velocity);
 
         super.update(elapsed);
     }
